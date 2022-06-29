69863b26d6671e8f0f28b35ad4916f74bf6d462c
abnazhor <jorbar982@outlook.com>
2022-06-30 00:37:37 +0200

    Fix tests

--- src/index.test.js
+++ src/index.test.js
@@ -9,23 +9,23 @@ describe("Palathea", () => {
   describe("is able to reply with a valid response", () => {
     const assistant = palathea(intents, handlers);
 
-    test("from an intent with simple responses", () => {
+    test("from an intent with simple responses", async () => {
       const input = "Cómo te llamas?";
-      const result = assistant.reply(input);
+      const result = await assistant.reply(input);
 
       expect(intents.whatsYourName.responses).to.include(result.content);
     });
 
-    test("from an intent with a handler", () => {
+    test("from an intent with a handler", async () => {
       const input = "Buenas noches Palathea";
-      const result = assistant.reply(input);
+      const result = await assistant.reply(input);
 
       expect(result).toStrictEqual(handlers[intents.greeting.handler]());
     });
 
-    test("from an intent simple response after failing to execute its handler", () => {
+    test("from an intent simple response after failing to execute its handler", async () => {
       const input = "cuentame un chiste";
-      const result = assistant.reply(input);
+      const result = await assistant.reply(input);
 
       const expected = {
         content: intents.joke.responses.at(0),
@@ -37,38 +37,38 @@ describe("Palathea", () => {
   });
 
   describe("replies with a fallback response", () => {
-    test("when there is not a valid enough response but a custom fallback response", () => {
+    test("when there is not a valid enough response but a custom fallback response", async () => {
       const assistant = palathea(intents, handlers);
 
       const input = "No, me gusta hacer muchas cosas";
-      const result = assistant.reply(input);
+      const result = await assistant.reply(input);
 
       expect(result).toStrictEqual(intents.fallback);
     });
 
-    test("when there is not a valid enough response nor custom fallback response", () => {
-        const assistant = palathea({...intents, fallback: null}, handlers);
+    test("when there is not a valid enough response nor custom fallback response", async () => {
+      const assistant = palathea({ ...intents, fallback: null }, handlers);
 
       const input = "No, me gusta hacer muchas cosas";
-      const result = assistant.reply(input);
+      const result = await assistant.reply(input);
 
-      expect(result).toStrictEqual(FALLBACK_RESPONSE)
+      expect(result).toStrictEqual(FALLBACK_RESPONSE);
     });
   });
 
   describe("handles conversational flow", () => {
-    test("returns the previousIntentId", () => {
+    test("returns the previousIntentId", async () => {
       const assistant = palathea(intents, handlers);
       const input = "Buenos días por la mañana";
 
-      assistant.reply(input);
+      await assistant.reply(input);
 
       const expected = "greeting";
 
       expect(assistant.getPreviousIntent()).toStrictEqual(expected);
     });
 
-    test("returns null if no replies have been thrown", () => {
+    test("returns null if no replies have been thrown", async () => {
       const assistant = palathea(intents, handlers);
 
       expect(assistant.getPreviousIntent()).toStrictEqual(null);
